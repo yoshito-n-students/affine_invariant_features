a66ef9edce5a72ad17b3335843c2d80370b0989d
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2016-12-31 10:14:01 +0900

    add ResultMatcher (never tested)

---
+++ include/affine_invariant_features/result_matcher.hpp
@@ -0,0 +1,95 @@
+#ifndef AFFINE_INVARIANT_FEATURES_RESULT_MATCHER
+#define AFFINE_INVARIANT_FEATURES_RESULT_MATCHER
+
+#include <vector>
+
+#include <affine_invariant_features/results.hpp>
+
+#include <opencv2/calib3d.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/features2d.hpp>
+
+namespace affine_invariant_features {
+
+class ResultMatcher {
+public:
+  ResultMatcher(const Results &train) : train_(train) {
+    switch (train_.normType) {
+    case cv::NORM_L1:
+      matcher_ = cv::DescriptorMatcher::create("BruteForce-L1");
+      break;
+    case cv::NORM_L2:
+      matcher_ = cv::DescriptorMatcher::create("FlannBased");
+      break;
+    case cv::NORM_HAMMING:
+      matcher_ = cv::DescriptorMatcher::create("BruteForce-Hamming");
+      break;
+    case cv::NORM_HAMMING2:
+      matcher_ = cv::DescriptorMatcher::create("BruteForce-Hamming2");
+      break;
+    }
+
+    CV_Assert(matcher_);
+
+    matcher_->add(train_.descriptors);
+    matcher_->train();
+  }
+
+  virtual ~ResultMatcher() {}
+
+  void match(const Results &query, cv::Matx33f &query2train,
+             std::vector< cv::DMatch > &matches) const {
+    // initiate outputs
+    query2train = cv::Matx33f::eye();
+    matches.clear();
+
+    // find the 1st & 2nd matches for each query result
+    std::vector< std::vector< cv::DMatch > > all_matches;
+    matcher_->knnMatch(query.descriptors, all_matches, 2);
+
+    // filter unique matches whose 1st is enough better than 2nd
+    std::vector< cv::DMatch > unique_matches;
+    for (std::vector< std::vector< cv::DMatch > >::const_iterator m = all_matches.begin();
+         m != all_matches.end(); ++m) {
+      if (m->size() < 2) {
+        continue;
+      }
+      if ((*m)[0].distance > 0.75 * (*m)[1].distance) {
+        continue;
+      }
+      unique_matches.push_back((*m)[0]);
+    }
+    if (unique_matches.size() < 4) {
+      // cv::findHomography requires 4 or more point pairs
+      return;
+    }
+
+    // further filter matches compatible to a query-to-train registration
+    std::vector< bool > mask;
+    {
+      std::vector< cv::Point2f > query_points;
+      std::vector< cv::Point2f > train_points;
+      for (std::vector< cv::DMatch >::const_iterator m = unique_matches.begin();
+           m != unique_matches.end(); ++m) {
+        query_points.push_back(query.keypoints[m->queryIdx].pt);
+        train_points.push_back(train_.keypoints[m->trainIdx].pt);
+      }
+      query2train = cv::findHomography(query_points, train_points, cv::RANSAC, 5., mask);
+    }
+
+    // pack the final matches
+    for (std::size_t i = 0; i < unique_matches.size(); ++i) {
+      if (!mask[i]) {
+        continue;
+      }
+      matches.push_back(unique_matches[i]);
+    }
+  }
+
+private:
+  const Results train_;
+  cv::Ptr< cv::DescriptorMatcher > matcher_;
+};
+}
+
+#endif
--- src/match_features.cpp
+++ src/match_features.cpp
@@ -1,14 +1,85 @@
 #include <iostream>
+#include <string>
 
 #include <affine_invariant_features/feature_parameters.hpp>
+#include <affine_invariant_features/target.hpp>
+#include <affine_invariant_features/results.hpp>
+#include <affine_invariant_features/result_matcher.hpp>
 
 #include <opencv2/core.hpp>
+#include <opencv2/features2d.hpp>
+
+namespace aif = affine_invariant_features;
+
+bool readEverything(const std::string &path, aif::TargetData &target_data, aif::Results &results) {
+  const cv::FileStorage file(path, cv::FileStorage::READ);
+
+  aif::TargetDescription target_desc;
+  file[target_desc.getDefaultName()] >> target_desc;
+  target_data = target_desc.toData();
+  if (target_data.image.empty()) {
+    std::cerr << "Could not load an image described in " << path << std::endl;
+    return false;
+  }
+
+  file[results.getDefaultName()] >> results;
+  if (results.keypoints.empty() || results.descriptors.empty()) {
+    std::cerr << "Could not load extracted features from " << path << std::endl;
+    return false;
+  }
+
+  return true;
+}
+
+cv::Mat shadeImage(const cv::Mat &src, const cv::Mat &mask) {
+  cv::Mat dst(src.clone());
+  if (!mask.empty()) {
+    cv::Mat shade(src / 2);
+    shade.copyTo(dst, mask);
+  }
+  return dst;
+}
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cout << "Usage: " << std::endl;
+
+  if (argc != 3) {
+    std::cout << "Usage: match_features <feature_file1> <feature_file2>" << std::endl;
+    std::cout << "Note: use extract_features to generate feature files" << std::endl;
     return 0;
   }
+
+  const std::string feature_path1(argv[1]);
+  const std::string feature_path2(argv[2]);
+
+  aif::TargetData target1;
+  aif::Results results1;
+  if (!readEverything(feature_path1, target1, results1)) {
+    return 1;
+  }
+  std::cout << "loaded " << results1.keypoints.size() << " feature points from " << feature_path1
+            << std::endl;
+
+  aif::TargetData target2;
+  aif::Results results2;
+  if (!readEverything(feature_path2, target2, results2)) {
+    return 1;
+  }
+  std::cout << "loaded " << results2.keypoints.size() << " feature points from " << feature_path2
+            << std::endl;
+
+  aif::ResultMatcher matcher(results2);
+  cv::Matx33f transform;
+  std::vector< cv::DMatch > matches;
+  matcher.match(results1, transform, matches);
+  std::cout << "found " << matches.size() << " matches" << std::endl;
+
+  const cv::Mat image1(shadeImage(target1.image, target1.mask));
+  const cv::Mat image2(shadeImage(target2.image, target2.mask));
+  cv::Mat image;
+  cv::drawMatches(image1, results1.keypoints, image2, results2.keypoints, matches, image);
+
+  cv::imshow("Matches", image);
+  cv::waitKey(0);
 
   return 0;
 }
