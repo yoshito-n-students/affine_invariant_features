7787f8764220497484648389dd28a535b63903b6
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2016-12-31 14:10:21 +0900

    remove a meaningless executable

--- src/test_affine_invariant_features.cpp
+++
@@ -1,8 +0,0 @@
-#include <affine_invariant_features/affine_invariant_feature.hpp>
-#include <affine_invariant_features/feature_parameters.hpp>
-#include <affine_invariant_features/target.hpp>
-#include <affine_invariant_features/results.hpp>
-
-int main(int argc, char *argv[]) {
-  return 0;
-}
