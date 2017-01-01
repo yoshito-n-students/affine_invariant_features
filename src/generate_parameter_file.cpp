386d5ff244519671bc47910a755486d7e8c5c536
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2017-01-01 10:57:09 +0900

    refactor generate_parameter_file

--- src/generate_parameter_file.cpp
+++ src/generate_parameter_file.cpp
@@ -6,27 +6,41 @@
 #include <opencv2/core.hpp>
 
 int main(int argc, char *argv[]) {
+
   namespace aif = affine_invariant_features;
 
-  if (argc != 3) {
-    std::cout << "Usage: generate_parameter_file <parameter_type> <parameter_file>" << std::endl;
-    std::cout << "Available parameter types: AKAZEParameters, BRISKParameters, SIFTParameters, ..."
-              << std::endl;
+  const cv::CommandLineParser args(
+      argc, argv,
+      "{ help | | }"
+      "{ @parameter-type | <none> | SIFTParameters, BRISKParameters, AKAZEParameters, ... }"
+      "{ @parameter-file | <none> | }");
+
+  if (args.has("help")) {
+    args.printMessage();
     return 0;
   }
 
-  const std::string type(argv[1]);
-  const std::string path(argv[2]);
+  const std::string type(args.get< std::string >("@parameter-type"));
+  const std::string path(args.get< std::string >("@parameter-file"));
+  if (!args.check()) {
+    args.printErrors();
+    return 1;
+  }
 
   const cv::Ptr< const aif::FeatureParameters > params(aif::createFeatureParameters(type));
   if (!params) {
-    std::cerr << "Could not create parameter set whose type is " << type << std::endl;
+    std::cerr << "Could not create a parameter set whose type is " << type << std::endl;
     return 1;
   }
 
   cv::FileStorage file(path, cv::FileStorage::WRITE);
+  if (!file.isOpened()) {
+    std::cerr << "Could not open or create " << path << std::endl;
+    return 1;
+  }
+  
   file << params->getDefaultName() << *params;
-  std::cout << "Wrote parameter set whose type is " << type << " to " << path << std::endl;
+  std::cout << "Wrote a parameter set whose type is " << type << " to " << path << std::endl;
 
   return 0;
 }
