853595b4ec5f71982d5f83c0978b15c3b8bcfec0
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2017-01-05 13:22:28 +0900

    refactor & support efficient detect() and compute() (never tested)

--- include/affine_invariant_features/affine_invariant_feature.hpp
+++ include/affine_invariant_features/affine_invariant_feature.hpp
@@ -25,7 +25,19 @@ private:
   // the private constructor. users must use create() to instantiate an AffineInvariantFeature
   AffineInvariantFeature(const cv::Ptr< cv::Feature2D > detector,
                          const cv::Ptr< cv::Feature2D > extractor)
-      : AffineInvariantFeatureBase(detector, extractor) {}
+      : AffineInvariantFeatureBase(detector, extractor) {
+    // generate parameters for affine invariant sampling
+    phi_params_.push_back(0.);
+    tilt_params_.push_back(1.);
+    for (int i = 1; i < 6; ++i) {
+      const double tilt(std::pow(2., 0.5 * i));
+      for (double phi = 0.; phi < 180.; phi += 72. / tilt) {
+        phi_params_.push_back(phi);
+        tilt_params_.push_back(tilt);
+      }
+    }
+    ntasks_ = phi_params_.size();
+  }
 
 public:
   virtual ~AffineInvariantFeature() {}
@@ -47,77 +59,169 @@ public:
   // overloaded functions from AffineInvariantFeatureBase or its base class
   //
 
-  // detect and compute affine invariant keypoints and descriptors
-  virtual void detectAndCompute(cv::InputArray image, cv::InputArray mask,
-                                std::vector< cv::KeyPoint > &keypoints, cv::OutputArray descriptors,
-                                bool useProvidedKeypoints = false) {
+  virtual void compute(cv::InputArray image, std::vector< cv::KeyPoint > &keypoints,
+                       cv::OutputArray descriptors) {
+    // extract the input
+    const cv::Mat image_mat(image.getMat());
+
+    // prepare outputs of following parallel processing
+    std::vector< std::vector< cv::KeyPoint > > keypoints_array(ntasks_, keypoints);
+    std::vector< cv::Mat > descriptors_array(ntasks_);
+
+    // bind each parallel task and arguments
+    ParallelTasks tasks(ntasks_);
+    for (int i = 0; i < ntasks_; ++i) {
+      tasks[i] = boost::bind(&AffineInvariantFeature::computeTask, this, boost::ref(image_mat),
+                             boost::ref(keypoints_array[i]), boost::ref(descriptors_array[i]),
+                             phi_params_[i], tilt_params_[i]);
+    }
+
+    // do parallel tasks
+    cv::parallel_for_(cv::Range(0, ntasks_), tasks);
+
+    // fill the final outputs
+    extendKeypoints(keypoints_array, keypoints);
+    extendDescriptors(descriptors_array, descriptors);
+  }
+
+  virtual void detect(cv::InputArray image, std::vector< cv::KeyPoint > &keypoints,
+                      cv::InputArray mask = cv::noArray()) {
     // extract inputs
     const cv::Mat image_mat(image.getMat());
     const cv::Mat mask_mat(mask.getMat());
 
-    // generate parameters for following parallel processing
-    std::vector< double > tilt_params;
-    std::vector< double > phi_params;
-    tilt_params.push_back(1.);
-    phi_params.push_back(0.);
-    for (int i = 1; i < 6; ++i) {
-      const double tilt(std::pow(2., 0.5 * i));
-      for (double phi = 0.; phi < 180.; phi += 72. / tilt) {
-        tilt_params.push_back(tilt);
-        phi_params.push_back(phi);
-      }
-    }
-
-    // prepare outputs of following parallel processing
-    const int ntasks(tilt_params.size());
-    std::vector< std::vector< cv::KeyPoint > > keypoints_array(ntasks);
-    std::vector< cv::Mat > descriptors_array(ntasks);
-    if (useProvidedKeypoints) {
-      std::fill(keypoints_array.begin(), keypoints_array.end(), keypoints);
-    }
+    // prepare an output of following parallel processing
+    std::vector< std::vector< cv::KeyPoint > > keypoints_array(ntasks_);
 
     // bind each parallel task and arguments
-    ParallelTasks tasks(ntasks);
-    for (int i = 0; i < ntasks; ++i) {
-      tasks[i] = boost::bind(&AffineInvariantFeature::detectAndComputeTask, this,
-                             boost::ref(image_mat), boost::ref(mask_mat),
-                             boost::ref(keypoints_array[i]), boost::ref(descriptors_array[i]),
-                             tilt_params[i], phi_params[i], useProvidedKeypoints);
+    ParallelTasks tasks(ntasks_);
+    for (std::size_t i = 0; i < ntasks_; ++i) {
+      tasks[i] = boost::bind(&AffineInvariantFeature::detectTask, this, boost::ref(image_mat),
+                             boost::ref(mask_mat), boost::ref(keypoints_array[i]), phi_params_[i],
+                             tilt_params_[i]);
     }
 
     // do parallel tasks
-    cv::parallel_for_(cv::Range(0, ntasks), tasks);
+    cv::parallel_for_(cv::Range(0, ntasks_), tasks);
 
-    // extend keypoints
-    keypoints.clear();
-    for (int i = 0; i < ntasks; ++i) {
-      keypoints.insert(keypoints.end(), keypoints_array[i].begin(), keypoints_array[i].end());
+    // fill the final output
+    extendKeypoints(keypoints_array, keypoints);
+  }
+
+  // detect and compute affine invariant keypoints and descriptors
+  virtual void detectAndCompute(cv::InputArray image, cv::InputArray mask,
+                                std::vector< cv::KeyPoint > &keypoints, cv::OutputArray descriptors,
+                                bool useProvidedKeypoints = false) {
+    // just compute descriptors if the keypoints is provided
+    if (useProvidedKeypoints) {
+      compute(image, keypoints, descriptors);
+      return;
     }
 
-    // extend descriptors
-    descriptors.create(keypoints.size(), descriptorSize(), descriptorType());
-    {
-      cv::Mat descriptors_mat(descriptors.getMat());
-      int rows(0);
-      for (int i = 0; i < ntasks; ++i) {
-        descriptors_array[i].copyTo(
-            descriptors_mat.rowRange(rows, rows + descriptors_array[i].rows));
-        rows += descriptors_array[i].rows;
-      }
+    // extract inputs
+    const cv::Mat image_mat(image.getMat());
+    const cv::Mat mask_mat(mask.getMat());
+
+    // prepare outputs of following parallel processing
+    std::vector< std::vector< cv::KeyPoint > > keypoints_array(ntasks_);
+    std::vector< cv::Mat > descriptors_array(ntasks_);
+
+    // bind each parallel task and arguments
+    ParallelTasks tasks(ntasks_);
+    for (std::size_t i = 0; i < ntasks_; ++i) {
+      tasks[i] =
+          boost::bind(&AffineInvariantFeature::detectAndComputeTask, this, boost::ref(image_mat),
+                      boost::ref(mask_mat), boost::ref(keypoints_array[i]),
+                      boost::ref(descriptors_array[i]), phi_params_[i], tilt_params_[i]);
     }
+
+    // do parallel tasks
+    cv::parallel_for_(cv::Range(0, ntasks_), tasks);
+
+    // fill the final outputs
+    extendKeypoints(keypoints_array, keypoints);
+    extendDescriptors(descriptors_array, descriptors);
   }
 
   virtual cv::String getDefaultName() const { return "AffineInvariantFeature"; }
 
 private:
-  void detectAndComputeTask(const cv::Mat &src_image, const cv::Mat &src_mask,
-                            std::vector< cv::KeyPoint > &keypoints, cv::Mat &descriptors,
-                            const double tilt, const double phi, const bool useProvidedKeypoints) {
-    // affine transformation to be applied to the given image
-    cv::Matx23f affine(cv::Matx23f::eye());
-
+  void computeTask(const cv::Mat &src_image, std::vector< cv::KeyPoint > &keypoints,
+                   cv::Mat &descriptors, const double phi, const double tilt) const {
     // apply the affine transformation to the image on the basis of the given parameters
     cv::Mat image(src_image.clone());
+    cv::Matx23f affine;
+    warpImage(image, affine, phi, tilt);
+
+    // apply the affine transformation to keypoints
+    transformKeypoints(keypoints, affine);
+
+    // extract descriptors on the skewed image and keypoints
+    CV_Assert(extractor_);
+    extractor_->compute(image, keypoints, descriptors);
+
+    // invert keypoints
+    cv::Matx23f invert_affine;
+    cv::invertAffineTransform(affine, invert_affine);
+    transformKeypoints(keypoints, invert_affine);
+  }
+
+  void detectTask(const cv::Mat &src_image, const cv::Mat &src_mask,
+                  std::vector< cv::KeyPoint > &keypoints, const double phi,
+                  const double tilt) const {
+    // apply the affine transformation to the image on the basis of the given parameters
+    cv::Mat image(src_image.clone());
+    cv::Matx23f affine;
+    warpImage(image, affine, phi, tilt);
+
+    // apply the affine transformation to the mask
+    cv::Mat mask(src_mask.empty() ? cv::Mat(src_image.size(), CV_8UC1, 255) : src_mask.clone());
+    warpMask(mask, affine, image.size());
+
+    // detect keypoints on the skewed image and mask
+    CV_Assert(detector_);
+    detector_->detect(image, keypoints, mask);
+
+    // invert keypoints
+    cv::Matx23f invert_affine;
+    cv::invertAffineTransform(affine, invert_affine);
+    transformKeypoints(keypoints, invert_affine);
+  }
+
+  void detectAndComputeTask(const cv::Mat &src_image, const cv::Mat &src_mask,
+                            std::vector< cv::KeyPoint > &keypoints, cv::Mat &descriptors,
+                            const double phi, const double tilt) {
+    // apply the affine transformation to the image on the basis of the given parameters
+    cv::Mat image(src_image.clone());
+    cv::Matx23f affine;
+    warpImage(image, affine, phi, tilt);
+
+    // if keypoints are not provided, first apply the affine transformation to the mask
+    cv::Mat mask(src_mask.empty() ? cv::Mat(src_image.size(), CV_8UC1, 255) : src_mask.clone());
+    warpMask(mask, affine, image.size());
+
+    // detect keypoints on the skewed image and mask
+    // and extract descriptors on the image and keypoints
+    if (detector_ == extractor_) {
+      CV_Assert(detector_);
+      detector_->detectAndCompute(image, mask, keypoints, descriptors, false);
+    } else {
+      CV_Assert(detector_);
+      CV_Assert(extractor_);
+      detector_->detect(image, keypoints, mask);
+      extractor_->compute(image, keypoints, descriptors);
+    }
+
+    // invert the positions of the detected keypoints
+    cv::Matx23f invert_affine;
+    cv::invertAffineTransform(affine, invert_affine);
+    transformKeypoints(keypoints, invert_affine);
+  }
+
+  static void warpImage(cv::Mat &image, cv::Matx23f &affine, const double phi, const double tilt) {
+    // initiate output
+    affine = cv::Matx23f::eye();
+
     if (phi != 0.) {
       // rotate the source frame
       affine = cv::getRotationMatrix2D(cv::Point2f(0., 0.), phi, 1.);
@@ -148,55 +252,58 @@ private:
       affine(0, 1) /= tilt;
       affine(0, 2) /= tilt;
     }
+  }
 
-    // detect features in the skewed image
-    if (useProvidedKeypoints) {
-      // if keypoints are provided, first apply the affine transformation to them
-      if (phi != 0. || tilt != 1.) {
-        for (std::vector< cv::KeyPoint >::iterator keypoint = keypoints.begin();
-             keypoint != keypoints.end(); ++keypoint) {
-          // convert cv::Point2f to cv::Mat (1x1,2ch) without copying data.
-          // this is required because cv::transform does not accept cv::Point2f.
-          cv::Mat pt(cv::Mat(keypoint->pt, false).reshape(2));
-          cv::transform(pt, pt, affine);
-        }
-      }
-
-      // extract descriptors on the skewed image and keypoints
-      CV_Assert(extractor_);
-      extractor_->compute(image, keypoints, descriptors);
-    } else {
-      // if keypoints are not provided, first apply the affine transformation to the mask
-      cv::Mat mask(src_mask.clone());
-      if (mask.empty()) {
-        mask = cv::Mat(src_image.size(), CV_8UC1, 255);
-      }
-      if (phi != 0. || tilt != 1.) {
-        cv::warpAffine(mask, mask, affine, image.size(), cv::INTER_NEAREST);
-      }
-
-      // detect keypoints on the skewed image and mask
-      // and extract descriptors on the image and keypoints
-      if (detector_ == extractor_) {
-        CV_Assert(detector_);
-        detector_->detectAndCompute(image, mask, keypoints, descriptors, useProvidedKeypoints);
-      } else {
-        CV_Assert(detector_);
-        CV_Assert(extractor_);
-        detector_->detect(image, keypoints, mask);
-        extractor_->compute(image, keypoints, descriptors);
-      }
+  static void warpMask(cv::Mat &mask, const cv::Matx23f &affine, const cv::Size size) {
+    if (affine == cv::Matx23f::eye()) {
+      return;
     }
+    cv::warpAffine(mask, mask, affine, size, cv::INTER_NEAREST);
+  }
 
-    // invert the positions of the detected keypoints
-    cv::Matx23f invert_affine;
-    cv::invertAffineTransform(affine, invert_affine);
+  static void transformKeypoints(std::vector< cv::KeyPoint > &keypoints,
+                                 const cv::Matx23f &affine) {
+    if (affine == cv::Matx23f::eye()) {
+      return;
+    }
     for (std::vector< cv::KeyPoint >::iterator keypoint = keypoints.begin();
          keypoint != keypoints.end(); ++keypoint) {
+      // convert cv::Point2f to cv::Mat (1x1,2ch) without copying data.
+      // this is required because cv::transform does not accept cv::Point2f.
       cv::Mat pt(cv::Mat(keypoint->pt, false).reshape(2));
-      cv::transform(pt, pt, invert_affine);
+      cv::transform(pt, pt, affine);
     }
   }
+
+  static void extendKeypoints(const std::vector< std::vector< cv::KeyPoint > > &src,
+                              std::vector< cv::KeyPoint > &dst) {
+    dst.clear();
+    for (std::size_t i = 0; i < src.size(); ++i) {
+      dst.insert(dst.end(), src[i].begin(), src[i].end());
+    }
+  }
+
+  void extendDescriptors(const std::vector< cv::Mat > &src, cv::OutputArray dst) const {
+    // create the output array
+    int nrows(0);
+    for (std::size_t i = 0; i < src.size(); ++i) {
+      nrows += src[i].rows;
+    }
+    dst.create(nrows, descriptorSize(), descriptorType());
+
+    // fill the output array
+    cv::Mat dst_mat(dst.getMat());
+    int rows(0);
+    for (std::size_t i = 0; i < src.size(); ++i) {
+      src[i].copyTo(dst_mat.rowRange(rows, rows + src[i].rows));
+      rows += src[i].rows;
+    }
+  }
+
+private:
+  std::vector< double > phi_params_;
+  std::vector< double > tilt_params_;
+  std::size_t ntasks_;
 };
 }
 
